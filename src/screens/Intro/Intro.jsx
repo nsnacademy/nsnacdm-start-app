4fff12fb4de5d293406bcdd19556bf972f893a92
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2025-12-28 16:08:54 +0500

    Add vercel routing

--- src/screens/Intro/Intro.jsx
+++ src/screens/Intro/Intro.jsx
@@ -1,7 +1,25 @@
+import { useTelegram } from "../../hooks/useTelegram";
+
 export default function Intro() {
+  const { user } = useTelegram();
+
   return (
-    <div>
+    <div style={{ padding: 20 }}>
       <h1>Intro works!</h1>
+
+      <div style={{ marginTop: 20, fontSize: 16 }}>
+        <p><b>Telegram User Test:</b></p>
+
+        {user ? (
+          <>
+            <p>ID: {user.id}</p>
+            <p>Имя: {user.first_name}</p>
+            <p>Username: {user.username ? user.username : "нет username"}</p>
+          </>
+        ) : (
+          <p>Нет данных о пользователе (вы не в Telegram WebApp)</p>
+        )}
+      </div>
     </div>
   );
 }
