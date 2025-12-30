d97c1f34c5c95c14cd0e04c9426afd5b62d10236
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2025-12-31 01:13:28 +0500

    Add Supabase client + auto user saving

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -192,7 +192,7 @@ export default function Home() {
         <div className="content">
           <img className="empty-img" src="/images/clipboard.png" alt="empty" />
           <h2>У вас пока нет задач</h2>
-          <p>Добавьте первую задачу, чтобы начать свой путь</p>
+          <p>Добавьте свою первую задачу</p>
           <button className="primary-btn">Добавить задачу</button>
         </div>
 
