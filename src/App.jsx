e2abd4bfee1af28278fcd45cf1d9320dae6162c1
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2025-12-30 12:25:20 +0500

    Add Supabase client + auto user saving

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Splash from "./screens/Splash/Splash";
 import Intro from "./screens/Intro/Intro";
 import StepIntro from "./screens/Step/StepIntro";
+import StepIntro2 from "./screens/Step/StepIntro2";
+
 
 export default function App() {
 
@@ -13,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Splash />} />
         <Route path="/intro" element={<Intro />} />
         <Route path="/step-intro" element={<StepIntro />} />   {/* 🔥 новый экран */}
+        <Route path="/step-intro-2" element={<StepIntro2 />} />
       </Routes>
     </BrowserRouter>
   );
