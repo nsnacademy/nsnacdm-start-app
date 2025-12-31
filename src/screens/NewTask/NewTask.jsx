efb0bd9765d35618a037bc39e6550b31884b6a95
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2025-12-31 21:46:26 +0500

    Add Supabase client + auto user saving

--- src/screens/NewTask/NewTask.jsx
+++ src/screens/NewTask/NewTask.jsx
@@ -496,7 +496,7 @@ export default function NewTask() {
                 id: Date.now(),
                 title: task,
                 time: selectedTime,
-                od: animatedReward,
+                od: animatedRewar,
                 hp: animatedHp,
               };
 
