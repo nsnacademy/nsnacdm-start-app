0cce1c6ace7ad281d5b1982120fe3455ee7f2566
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2025-12-31 21:41:18 +0500

    Add Supabase client + auto user saving

--- src/store/taskStore.js
+++ src/store/taskStore.js
@@ -1,20 +1,28 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useTaskStore = create((set) => ({
-  tasks: [],
-
-  addTask: (task) =>
-    set((state) => ({
-      tasks: [...state.tasks, task],
-    })),
-
-  removeTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.filter((t) => t.id !== id),
-    })),
-
-  clearTasks: () =>
-    set(() => ({
+export const useTaskStore = create(
+  persist(
+    (set) => ({
       tasks: [],
-    })),
-}));
+
+      addTask: (task) =>
+        set((state) => ({
+          tasks: [...state.tasks, task],
+        })),
+
+      removeTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.filter((t) => t.id !== id),
+        })),
+
+      clearTasks: () =>
+        set(() => ({
+          tasks: [],
+        })),
+    }),
+    {
+      name: "tasks-storage", // ← имя хранилища в localStorage
+    }
+  )
+);
